9b61db17299d5406a7aaee8af81d60ee205510c0
GCGgg000 <gcg007@bluewin.ch>
2026-09-07 15:02:30 +0200

    Enhance test for history by adding validation for history appending in CalculatorService

--- backend/tests/test_history.py
+++ backend/tests/test_history.py
@@ -1,6 +1,14 @@
 from app.calculator import add
 
 def test_append_history():
-  result = add(10, 20)
+  service = CalculatorService()
 
-  # add to list 
+  listbefore = List(service.history)
+  result = str(add(10, 20))
+  service.append_history(("add",10,20,result))
+
+  listafter = List(service.history)
+
+  assert len(listafter) == len(listbefore) + 1
+  
+  
